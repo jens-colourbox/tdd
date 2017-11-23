24380f653d1c5b3a085dbf2b0efeea5408f4cf56
jens-colourbox <jens@colourbox.com>
2017-11-23 16:00:24 +0100

    let's add one more test

--- tests/fraction-calculator.test.js
+++ tests/fraction-calculator.test.js
@@ -2,16 +2,24 @@ const Fraction = require('../src/fraction');
 const FractionCalculator = require('../src/fraction-calculator');
 
 describe('adding fractions using the calculator', () => {
-
+  const fractionCalculator = new FractionCalculator();
+  
   test('7/3 and 4/5 is ', () => {
-    const fractionCalculator = new FractionCalculator();
-    const d7n3 = new Fraction(7, 3);
-    const d4n5 = new Fraction(4, 5);
-
-    const result = fractionCalculator.add(d7n3, d4n5);
+    let d7n3 = new Fraction(7, 3);
+    let d4n5 = new Fraction(4, 5);
+    let result = fractionCalculator.add(d7n3, d4n5);
 
     expect(result.numerator).toBe(47);
     expect(result.denominator).toBe(15);
+  });
+
+  test('12/7 and 2/3 is ', () => {
+    let d12n7 = new Fraction(12, 7);
+    let d2n3 = new Fraction(2, 3);
+    let result = fractionCalculator.add(d12n7, d2n3);
+
+    expect(result.numerator).toBe(50);
+    expect(result.denominator).toBe(21);
   });
 
 });
